7d792e231e88b1b39c71a11967d6b1c990ad1d52
flamrdevs <flamrdevs@gmail.com>
2022-12-09 18:26:49 +0700

    Implement polymorphic component

--- packages/react/src/index.test.tsx
+++ packages/react/src/index.test.tsx
@@ -30,6 +30,11 @@ describe("klassed", async () => {
           block: "display-block",
           none: "display-none",
         },
+        order: {
+          1: "order-1",
+          2: "order-2",
+          3: "order-3",
+        },
       },
       defaultVariants: {
         display: "block",
@@ -48,6 +53,11 @@ describe("klassed", async () => {
         display: {
           block: "display-block",
           none: "display-none",
+        },
+        order: {
+          1: "order-1",
+          2: "order-2",
+          3: "order-3",
         },
       },
       defaultVariants: {
@@ -69,6 +79,11 @@ describe("klassed", async () => {
         display: {
           block: "display-block",
           none: "display-none",
+        },
+        order: {
+          1: "order-1",
+          2: "order-2",
+          3: "order-3",
         },
       },
       defaultVariants: {
@@ -107,11 +122,13 @@ describe("klassed", async () => {
           block: "display-block",
           none: "display-none",
         },
+        order: {
+          1: "order-1",
+          2: "order-2",
+          3: "order-3",
+        },
         className: {
           will: "not-work",
-        },
-        children: {
-          haha: "lol",
         },
       },
       defaultVariants: {
@@ -119,9 +136,133 @@ describe("klassed", async () => {
       },
     });
 
-    render(<Div data-testid="div" enable className="extra class" {...{ out1: "out1", out2: "out2", out3: "out3" }} children="haha" />);
+    const Button = klassed(
+      "button",
+      {
+        base: "btn",
+        variants: {
+          color: {
+            red: "btn--red",
+            green: "btn--green",
+            blue: "btn--blue",
+          },
+          outline: {
+            true: "btn--outline",
+          },
+        },
+        defaultVariants: {
+          color: "blue",
+        },
+        compoundVariants: [
+          {
+            variant: {
+              color: "red",
+              outline: true,
+            },
+            classes: "btn--outline-red",
+          },
+          {
+            variant: {
+              color: "green",
+              outline: true,
+            },
+            classes: "btn--outline-green",
+          },
+          {
+            variant: {
+              color: "blue",
+              outline: true,
+            },
+            classes: "btn--outline-blue",
+          },
+        ],
+      },
+      {
+        defaultProps: {
+          type: "button",
+        },
+      }
+    );
 
-    expect(screen.getByTestId(/div/i)).toBeDefined();
-    expect(screen.getByTestId(/div/i).classList.toString()).toEqual("div enable-true display-block lol extra class");
+    const LinkComponent = React.forwardRef<HTMLAnchorElement, JSX.IntrinsicElements["a"]>((props, ref) => <a {...props} ref={ref} />);
+    const ButtonLink = klassed(LinkComponent, Button.klass.options, { defaultProps: { href: "/" } });
+
+    render(
+      <>
+        <Div data-testid="div" enable className="extra class">
+          div
+        </Div>
+        <Button data-testid="btn-red" color="red" outline>
+          btn-red
+        </Button>
+        <Button data-testid="btn-green" color="green" outline>
+          btn-green
+        </Button>
+        <Button data-testid="btn-blue" color="blue" outline>
+          btn-blue
+        </Button>
+
+        <Button data-testid="btn-red-as-a" as="a" color="red" outline>
+          btn-red-as-a
+        </Button>
+        <Button data-testid="btn-green-as-a" as="a" color="green" outline>
+          btn-green-as-a
+        </Button>
+        <Button data-testid="btn-blue-as-a" as="a" color="blue" outline>
+          btn-blue-as-a
+        </Button>
+
+        <ButtonLink data-testid="btn-link-red" color="red" outline>
+          btn-link-red
+        </ButtonLink>
+        <ButtonLink data-testid="btn-link-green" color="green" outline>
+          btn-link-green
+        </ButtonLink>
+        <ButtonLink data-testid="btn-link-blue" color="blue" outline>
+          btn-link-blue
+        </ButtonLink>
+      </>
+    );
+
+    expect(screen.getByTestId("div")).toBeDefined();
+    expect(screen.getByTestId("div").tagName).toEqual("DIV");
+    expect(screen.getByTestId("div").classList.toString()).toEqual("div enable-true display-block extra class");
+    expect(screen.getByTestId("div").textContent).toEqual("div");
+
+    function expectButton(color: string) {
+      const element = screen.getByTestId(`btn-${color}`);
+      expect(element).toBeDefined();
+      expect(element.tagName).toEqual("BUTTON");
+      expect(element.classList.toString()).toEqual(`btn btn--${color} btn--outline btn--outline-${color}`);
+      expect(element.getAttribute("type")).toEqual("button");
+      expect(element.textContent).toEqual(`btn-${color}`);
+    }
+    expectButton("red");
+    expectButton("green");
+    expectButton("blue");
+
+    function expectButtonAsA(color: string) {
+      const element = screen.getByTestId(`btn-${color}-as-a`);
+      expect(element).toBeDefined();
+      expect(element.tagName).toEqual("A");
+      expect(element.classList.toString()).toEqual(`btn btn--${color} btn--outline btn--outline-${color}`);
+      expect(element.getAttribute("type")).toEqual("button");
+      expect(element.textContent).toEqual(`btn-${color}-as-a`);
+    }
+    expectButtonAsA("red");
+    expectButtonAsA("green");
+    expectButtonAsA("blue");
+
+    function expectButtonLink(color: string) {
+      const element = screen.getByTestId(`btn-link-${color}`);
+      expect(element).toBeDefined();
+      expect(element.tagName).toEqual("A");
+      expect(element.classList.toString()).toEqual(`btn btn--${color} btn--outline btn--outline-${color}`);
+      expect(element.getAttribute("href")).toEqual("/");
+      expect(element.textContent).toEqual(`btn-link-${color}`);
+    }
+    expectButtonLink("red");
+    expectButtonLink("green");
+    expectButtonLink("blue");
   });
 });
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import type { ComponentProps, ComponentPropsWithRef, ComponentPropsWithoutRef, ElementType, ForwardRefExoticComponent } from "react";
+import type { ComponentPropsWithRef, ComponentPropsWithoutRef, ElementType, PropsWithChildren, ReactElement } from "react";
 
 import { klass } from "@klass/core";
 import type { ClassValue, VariantsSchema, KlassOptions, KlassFn, VariantsOf } from "@klass/core";
 
+type AsProp<C extends ElementType> = { as?: C };
+
+type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P);
+
+type PolymorphicComponentProp<C extends ElementType, Props = {}> = PropsWithChildren<Props & AsProp<C>> &
+  Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+type PolymorphicRef<C extends ElementType> = ComponentPropsWithRef<C>["ref"];
+
+type PolymorphicComponentPropWithRef<C extends ElementType, Props = {}> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
+
 function withKlassProps<T extends VariantsSchema, P extends { className?: ClassValue } & VariantsOf<KlassFn<T>>>(
   klassFn: KlassFn<T>,
-  props: P,
+  props: Omit<P, "as">,
   keys: (keyof Exclude<T, "className">)[]
 ) {
   const { className, ...rest } = props;
@@ -25,28 +36,38 @@ function withKlassProps<T extends VariantsSchema, P extends { className?: ClassV
   } as Omit<P, "className" | keyof Omit<VariantsOf<KlassFn<T>>, "className">> & { className: string };
 }
 
-type KlassedComponent<E extends ElementType, T extends VariantsSchema> = ForwardRefExoticComponent<
-  Omit<ComponentProps<E> & VariantsOf<KlassFn<T>>, "className"> & { className?: ClassValue }
-> & {
+type KlassedComponent<E extends ElementType, T extends VariantsSchema> = {
+  <C extends ElementType = E>(
+    props: PolymorphicComponentPropWithRef<C, Omit<VariantsOf<KlassFn<T>>, "className">> & { className?: ClassValue }
+  ): ReactElement | null;
+} & {
   klass: KlassFn<T>;
 };
 
-function klassed<E extends ElementType, T extends VariantsSchema>(Element: E, options: KlassOptions<T>): KlassedComponent<E, T> {
+function klassed<E extends ElementType, T extends VariantsSchema>(
+  element: E,
+  options: KlassOptions<T>,
+  setup?: { defaultProps: PolymorphicComponentPropWithRef<E, {}> }
+): KlassedComponent<E, T> {
   const klassFn = klass<T>(options);
   const keys = Object.keys(options.variants).filter((el) => el !== "className") as unknown as (keyof Exclude<T, "className">)[];
 
-  const Component = React.forwardRef(
-    (
-      props: Omit<ComponentPropsWithoutRef<E> & VariantsOf<KlassFn<T>>, "className"> & { className?: ClassValue },
-      ref?: ComponentPropsWithRef<E>["ref"]
-    ) => {
-      return <Element {...(withKlassProps(klassFn, props, keys) as unknown as any)} ref={ref} />;
-    }
-  );
+  const defaultProps = setup?.defaultProps || ({} as PolymorphicComponentPropWithRef<E, {}>);
 
-  (Component as KlassedComponent<E, T>).klass = klassFn;
+  const Component = <C extends ElementType = E>(
+    props: PolymorphicComponentPropWithRef<C, Omit<VariantsOf<KlassFn<T>>, "className">> & { className?: ClassValue },
+    ref?: PolymorphicRef<C>
+  ) => {
+    const { as: As = element, ...rest } = props;
 
-  return Component as KlassedComponent<E, T>;
+    return <As {...defaultProps} {...(withKlassProps(klassFn, rest, keys) as unknown as any)} ref={ref} />;
+  };
+
+  const ComponentWithRef: Omit<KlassedComponent<E, T>, "klass"> = React.forwardRef<any, any>(Component);
+
+  (ComponentWithRef as KlassedComponent<E, T>).klass = klassFn;
+
+  return ComponentWithRef as KlassedComponent<E, T>;
 }
 
 export { klassed };
