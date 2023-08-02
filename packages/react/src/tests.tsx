e665e8abec813bbe973ea4c0f8797c274ffdb9f6
flamrdevs <flamrdevs@gmail.com>
2023-08-02 10:37:22 +0700

    test(packages/*): fix have property expects

--- packages/react/src/tests.tsx
+++ packages/react/src/tests.tsx
@@ -9,16 +9,16 @@ import type { KlassedComponent, ReklassedComponent } from "./types";
 
 const expectVariantFn = <T extends VariantsSchema[string]>(variantFn: VariantFn<T>) => {
   expect(variantFn).toBeTypeOf("function");
-  expect(variantFn).toHaveProperty("options");
+  expect(variantFn).toHaveProperty("o");
   expect(variantFn.o).toBeTypeOf("object");
 };
 
 const expectKlassFn = <T extends VariantsSchema>(klassFn: KlassFn<T>, options: { keys: (keyof T)[] }) => {
   expect(klassFn).toBeTypeOf("function");
-  expect(klassFn).toHaveProperty("options");
+  expect(klassFn).toHaveProperty("o");
   expect(klassFn.o).toBeTypeOf("object");
 
-  expect(klassFn).toHaveProperty("variant");
+  expect(klassFn).toHaveProperty("v");
   expect(klassFn.v).toBeTypeOf("object");
   expect(klassFn.vk).toEqual(options.keys);
   options.keys.forEach((key) => {
@@ -34,16 +34,16 @@ const expectKlassedComponent = <T extends VariantsSchema>(klassedComponent: Klas
 
 const expectRevariantFn = <C extends ConditionSchema, T extends VariantsSchema[string]>(revariantFn: RevariantFn<C, T>) => {
   expect(revariantFn).toBeTypeOf("function");
-  expect(revariantFn).toHaveProperty("options");
+  expect(revariantFn).toHaveProperty("o");
   expect(revariantFn.o).toBeTypeOf("object");
 };
 
 const expectReklassFn = <C extends ConditionSchema, T extends VariantsSchema>(reklassFn: ReklassFn<C, T>, options: { keys: (keyof T)[] }) => {
   expect(reklassFn).toBeTypeOf("function");
-  expect(reklassFn).toHaveProperty("options");
+  expect(reklassFn).toHaveProperty("o");
   expect(reklassFn.o).toBeTypeOf("object");
 
-  expect(reklassFn).toHaveProperty("revariant");
+  expect(reklassFn).toHaveProperty("rv");
   expect(reklassFn.rv).toBeTypeOf("object");
   expect(reklassFn.rvk).toEqual(options.keys);
   options.keys.forEach((key) => {
